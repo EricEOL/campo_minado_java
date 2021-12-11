32fce39edce0965dce57060cdc6c2d1c00234575
EricEOL <lopes.eric051@gmail.com>
2021-12-11 15:38:10 -0300

    Continuando o projeto

--- src/br/com/cod3r/cm/modelo/Campo.java
+++ src/br/com/cod3r/cm/modelo/Campo.java
@@ -86,6 +86,44 @@ public class Campo {
 		return !this.isAberto();
 	}
 	
+	public int getColuna() {
+		return coluna;
+	}
+	
+	public int getLinha() {
+		return linha;
+	}
+	
+	boolean objetivoAlcancado() {
+		boolean desvendado = !minado && aberto;
+		boolean protegido = minado && marcado;
+		
+		return desvendado || protegido;
+	}
+	
+	long minasNaVizinhanca() {
+		return this.vizinhos.stream().filter(v -> v.minado).count();
+	}
+	
+	void reiniciar() {
+		this.aberto = false;
+		this.minado = false;
+		this.marcado = false;
+	}
+	
+	public String toString() {
+		if(marcado) {
+			return "x";
+		} else if(aberto && minado) {
+			return "*";
+		} else if(aberto && minasNaVizinhanca() > 0) {
+			return Long.toString(minasNaVizinhanca());
+		} else if(aberto) {
+			return " ";
+		} else {
+			return "?";
+		}
+	}
 }
 
 
